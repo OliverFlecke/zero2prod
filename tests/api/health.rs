2d5054723008eb23509ada3b379de30de9786020
Oliver Fleckenstein <oliverfl@live.dk>
2023-11-16 21:39:19 +0000

    test: Integration test for `/status` endpoint

--- tests/api/health.rs
+++ tests/api/health.rs
@@ -51,3 +51,35 @@ async fn info_endpoint_gives_build_info() {
         .and_then(|x| NaiveDateTime::parse_from_str(x, "%Y-%m-%dT%H:%M:%S%.f").ok())
         .is_some());
 }
+
+#[tokio::test]
+async fn status_endpoint_returns_up_when_both_services_are_up() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act
+    let response = app
+        .api_client()
+        .get(app.at_url("/status"))
+        .send()
+        .await
+        .expect("Request failed");
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.text().await.expect("unable to read body");
+    let body: Value = serde_json::from_str(&body).expect("unable to parse json");
+    assert_eq!(
+        body.get("is_db_connected")
+            .and_then(|x| x.as_bool())
+            .unwrap(),
+        true
+    );
+    assert_eq!(
+        body.get("is_redis_connected")
+            .and_then(|x| x.as_bool())
+            .unwrap(),
+        true
+    );
+}
