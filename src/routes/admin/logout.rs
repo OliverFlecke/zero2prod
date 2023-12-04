6f4a4026afe207fe86ae1e17d1a99c080b6627e3
Oliver Fleckenstein <oliverfl@live.dk>
2023-12-04 15:21:56 +0000

    test: Add integration test to verify logout is clearing session

--- src/routes/admin/logout.rs
+++ src/routes/admin/logout.rs
@@ -1,8 +1,12 @@
-use crate::{service::flash_message::FlashMessage, state::session::Session};
+use crate::{
+    service::flash_message::FlashMessage,
+    state::{session::Session, AppState},
+};
 use axum::response::{IntoResponse, Redirect};
 
 /// Log the user out of the current session.
 #[tracing::instrument(name = "Log out", skip(session, flash))]
+#[axum::debug_handler(state = AppState)]
 pub async fn log_out(flash: FlashMessage, session: Session) -> impl IntoResponse {
     session.log_out();
     let flash = flash.set_message("You have successfully logged out.".to_string());
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -48,3 +48,34 @@ async fn redirect_to_admin_dashboard_after_login_success() {
     let html_page = app.get_admin_dashboard_html().await;
     assert!(html_page.contains(&format!("Welcome {}", app.test_user().username())));
 }
+
+#[tokio::test]
+async fn redirect_to_login_after_successful_logout() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act - Part 1 - Login
+    let login_body = serde_json::json!({
+        "username": app.test_user().username(),
+        "password": app.test_user().password(),
+    });
+    let response = app.post_login(&login_body).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    // Act - Part 2 - Follow the redirect
+    let html_page = app.get_admin_dashboard_html().await;
+    assert!(html_page.contains(&format!("Welcome {}", app.test_user().username())));
+
+    // Act - Part 3 - Logout
+    let response = app
+        .api_client()
+        .post(app.at_url("/admin/logout"))
+        .send()
+        .await
+        .expect("Logout request failed");
+    assert_is_redirect_to(&response, "/login");
+
+    // Act - Part 3 - Ensure request to dashboard redirects to login
+    let response = app.get_admin_dashboard().await;
+    assert_is_redirect_to(&response, "/login");
+}
